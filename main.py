b88f2b50883ceb1c3f163be909c34b3ada99fc7e
Om_711 <154738879+Om-711@users.noreply.github.com>
2025-10-13 01:12:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_origins=[
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://apnabzaar.netlify.app/'
+        'https://apnabzaar.netlify.app'
     ], 
 
     allow_credentials=True,
